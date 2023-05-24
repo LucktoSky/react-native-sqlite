276e934f8465b229811c557d49ec3278abcf68e5
LucktoSky <cloud.fist123@gmail.com>
2023-05-24 16:22:36 +0900

    BREAKING CHANGE

--- App.js
+++ App.js
@@ -3,20 +3,67 @@ import * as React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import HomeScreen from './pages/HomeScreen';
 import RegisterUser from './pages/Register';
+import UpdateUser from './pages/UpdateUser';
+import ViewUser from './pages/ViewUser';
 import ViewAllUser from './pages/ViewAllUser';
+import DeleteUser from './pages/DeleteUser';
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomeScreen">
+       <Stack.Navigator initialRouteName="HomeScreen">
         <Stack.Screen
           name="HomeScreen"
           component={HomeScreen}
           options={{
             title: 'Home', //Set Header Title
+            headerStyle: {
+              backgroundColor: '#f4511e', //Set Header color
+            },
+            headerTintColor: '#fff', //Set Header text color
+            headerTitleStyle: {
+              fontWeight: 'bold', //Set Header text style
+            },
+          }}
+        />
+        <Stack.Screen
+          name="View"
+          component={ViewUser}
+          options={{
+            title: 'View User', //Set Header Title
+            headerStyle: {
+              backgroundColor: '#f4511e', //Set Header color
+            },
+            headerTintColor: '#fff', //Set Header text color
+            headerTitleStyle: {
+              fontWeight: 'bold', //Set Header text style
+            },
+          }}
+        />
+        <Stack.Screen
+          name="ViewAll"
+          component={ViewAllUser}
+          options={{
+            title: 'View Users', //Set Header Title
+            headerStyle: {
+              backgroundColor: '#f4511e', //Set Header color
+            },
+            headerTintColor: '#fff', //Set Header text color
+            headerTitleStyle: {
+              fontWeight: 'bold', //Set Header text style
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Update"
+          component={UpdateUser}
+          options={{
+            title: 'Update User', //Set Header Title
             headerStyle: {
               backgroundColor: '#f4511e', //Set Header color
             },
@@ -41,10 +88,10 @@ export default function App() {
           }}
         />
         <Stack.Screen
-          name="ViewAll"
-          component={ViewAllUser}
+          name="Delete"
+          component={DeleteUser}
           options={{
-            title: 'View Users', //Set Header Title
+            title: 'Delete User', //Set Header Title
             headerStyle: {
               backgroundColor: '#f4511e', //Set Header color
             },
@@ -54,7 +101,6 @@ export default function App() {
             },
           }}
         />
-        
       </Stack.Navigator>
       <Text
           style={{
